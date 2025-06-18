e9722261fcf775e09e59aa3055503aaa697c8b89
abhiit89 <abhi.aec89@gmail.com>
2025-06-19 00:50:29 +0530

    word count

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
 
+def get_book_words(file_content):
+    return len(file_content.split())
 
 def main():
     result = get_book_text('books/frankenstein.txt')
-    print(result)
+    num_words = get_book_words(result)
+    print(f"{num_words} words found in the document")
 
 main()
